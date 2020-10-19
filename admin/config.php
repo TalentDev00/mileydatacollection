be7d2dcc4c7800e2155df9c9a377b319f8b8db9a
giordanoplas <72947120+giordanoplas@users.noreply.github.com>
2020-10-19 18:05:03 -0400

    Upload the admin folder
    This are the php files from connection and html_dom

---
+++ admin/config.php
@@ -0,0 +1,22 @@
+<?php
+
+define('RUTA', 'http://localhost:8080/projects/danny_scraping/');
+define('SCRAP_URL', 'https://www.imdb.com/search/title/?year=2020,2020&title_type=feature&explore=has');
+
+//Connection localhost
+$bd_config = array(
+    'host' => 'localhost',
+    'database' => 'miley',
+    'user' => 'root',
+    'pass' => ''
+);
+
+/*
+//Connection SSH MYSQL
+$bd_config = array(
+    'host' => 'localhost',
+    'database' => 'miley',
+    'user' => 'miley',
+    'pass' => 'VjV2eu7Hbn'
+);
+*/
